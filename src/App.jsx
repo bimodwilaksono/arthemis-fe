5eabeb0afcf90210203eb660c4edc7f5dd4a5b7d
vrima <vrimaratna@gmail.com>
2023-05-16 13:44:19 +0700

    Create Loading

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { constants } from "./constants/index.js";
 import { Register } from "./pages/Register/Register.jsx";
 import { ThemeProvider } from "./ThemeProvider";
 import Layouts from "./components/Layouts";
+import PreLoading from "./components/Loading/index.jsx"
+import {Suspense} from "react";
 
 const { ROUTES } = constants;
 
 function App() {
     return (
         <ThemeProvider>
-            <Routes>
-                <Route exact path={ROUTES.LOGIN} element={<Login />} />
-                <Route exact path={ROUTES.REGISTER} element={<Register />} />
-                <Route element={<Layouts />} path={ROUTES.DASHBOARD} />
-            </Routes>
+            <Suspense fallback={<PreLoading/>}>
+                <Routes>
+                    <Route exact path={ROUTES.LOGIN} element={<Login />} />
+                    <Route exact path={ROUTES.REGISTER} element={<Register />} />
+                    <Route element={<Layouts />} path={ROUTES.DASHBOARD} />
+                </Routes>
+            </Suspense>
         </ThemeProvider>
     );
 }
---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,13 @@
+import Lottie from "lottie-react";
+
+import loading from "../../../assets/loading-13.json"
+
+function PreLoading() {
+    return(
+        <div>
+            <Lottie animationData={loading}/>
+        </div>
+    )
+}
+
+export default PreLoading
